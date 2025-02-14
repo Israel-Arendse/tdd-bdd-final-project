4d1f0ec1af9d7a8fc41a26bd5553981890cf7d58
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-14 13:01:17 -0500

    Implement 'Update a Product' testcase in tests/test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -175,6 +175,21 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         self.assertEqual(data["name"], test_product.name)
 
+    def test_update_product(self):
+        """It should update an existing Prdouct"""
+        # create a product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the product
+        new_product = response.get_json()
+        new_product["description"] = "unknown"
+        response = self.client.put(f"BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["description"], "unknown")
+
     ######################################################################
     # Utility functions
     ######################################################################
