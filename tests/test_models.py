2927c8da2591d0fd4dcb7cce8805b9a3b3711c4a
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-14 03:02:32 -0500

    Add  'UPDATE A PRODUCT' testcase in tests/test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,6 +104,7 @@ class TestProductModel(unittest.TestCase):
     
     # ADD YOUR TEST CASES HERE
     
+    # TEST-CASE: READ A PRODUCT
     def test_read_a_product(self):
         """It should Read a Product"""
         product = ProductFactory()
@@ -116,3 +117,24 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found_product.name, product.name)
         self.assertEqual(found_product.description, product.description)
         self.assertEqual(found_product.price, product.price)
+
+    # TEST-CASE: UPDATE A PRODUCT
+    def test_update_a_product(self):
+        """It should Update a Product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        # Change it and save it
+        product.description = "testing"
+        original_id = product.id
+        product.update()
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "testing")
+        # Fetch it back and make sure that the id hasn't changed
+        # but the data did change
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        self.assertEqual(products[0].id, original_id)
+        self.assertEqual(products[0].description, "testing")
+        
