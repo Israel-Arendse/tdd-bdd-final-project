876b1f5bdfcab897c6da959a69b4cbc0a3477698
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-14 03:25:04 -0500

    Add 'Find By Name' testcase  in tests/test_modesl.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -158,3 +158,16 @@ class TestProductModel(unittest.TestCase):
             product.create()
         products = Product.all()
         self.assertEqual(len(products), 5)
+
+    # TEST-CASE: FIND BY NAME
+    def test_find_by_name(self):
+        """It should find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name === name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
