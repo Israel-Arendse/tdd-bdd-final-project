5979b11f478e4bbc2372b73492db8a3f5f06bae9
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-14 03:38:41 -0500

    Add 'Find By Availability' testcase  in tests/test_modesl.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -171,3 +171,16 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.name, name)
+
+    # TEST-CASE: FIND BY AVAILABILITY
+    def test_find_by_availability(self):
+        """It should find Products by Availability"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        available = products.[0].available
+        count = len([product for product in products if product.available == available])
+        found = Product.test_find_by_availability(available)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.available, available)
