e87f62e9dc4466635b4d13e187acae6be78c9114
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-17 14:10:41 -0500

    Add 'I should not see {name}' step definition in web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -120,6 +120,11 @@ def step_impl(context, name):
     )
     assert(found)
 
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert(name not in element.text)
+    
 @then('I should see the message "{message}"')
 def step_impl(context, message):
     found = WebDriverWait(context.driver, context.wait_seconds).until(
