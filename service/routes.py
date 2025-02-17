996a8f52dfeebba15e0c464925c2c0dc57713050
Israel-Arendse <israelarendsefortnite@gmail.com>
2025-02-17 05:06:39 -0500

    Update 'List Products' code with find by availability filter in service/routes.py

--- service/routes.py
+++ service/routes.py
@@ -100,9 +100,12 @@ def create_products():
 def list_products():
     """Returns a list of Products"""
     app.logger.info("Request to list Products...")
+
     products = []
     name = request.args.get("name")
     category = request.args.get("category")
+    available = request.args.get("available")
+
     if name:
         app.logger.info("Find by name: %s", name)
         products = Product.find_by_name(name)
@@ -111,9 +114,15 @@ def list_products():
         # create enum from string
         category_value = getattr(Category, category.upper())
         products = Product.find_by_category(category_value)
+    elif available:
+        app.logger.info("Find by available: %s", available)
+        # create bool from string
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)avail
     else:
         app.logger.info("Find all")
         products = Product.all()
+        
     results = [product.serialize() for product in products]
     app.logger.info("[%s] Products returned", len(results))
     return results, status.HTTP_200_OK
