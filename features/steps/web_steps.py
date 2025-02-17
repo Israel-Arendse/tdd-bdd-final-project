424b4aef73e11df2d5bb0db595bfc86e5d285842
Israel-Arendse <isrealarendsefortnite@gmail.com>
2025-02-17 13:08:34 -0500

    Remove redundant step in steps/web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -120,11 +120,6 @@ def step_impl(context, name):
     )
     assert(found)
 
-@then('I should not see "{name}" in the results')
-def step_impl(context, name):
-    element = context.driver.find_element(By.ID, 'search-results')
-    assert(name not in element.text)
-
 @then('I should see the message "{message}"')
 def step_impl(context, message):
     found = WebDriverWait(context.driver, context.wait_seconds).until(
